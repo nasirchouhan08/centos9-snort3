973c639b15fedb4f812b04925b67bd46d8397110
Leslie <lesysec@gmail.com>
2022-11-25 16:35:13 +0900

    Add snort on /usr/bin

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-# CentOS 9 Stream with Snort3
+# CentOS 9 Stream with Snort3 Installer
 
 ## Ready to run
 sudo ldconfig
@@ -17,15 +17,19 @@ sudo dnf install -y hyperscan hyperscan-devel
 cd ~/sources && curl -Lo flatbuffers-22.11.23.tar.gz https://github.com/google/flatbuffers/archive/v22.11.23.tar.gz && tar xf flatbuffers-22.11.23.tar.gz
 mkdir -p ~/sources/fb-build && cd ~/sources/fb-build && sudo cmake ~/sources/flatbuffers-22.11.23 && sudo make -j$(nproc) && sudo make -j$(nproc) install && sudo ldconfig && flatc --version
 cd ~/sources && curl -Lo cert-forensics-tools-release-el9.rpm https://forensics.cert.org/cert-forensics-tools-release-el9.rpm && sudo rpm -Uvh cert-forensics-tools-release*rpm && sudo dnf --enablerepo=forensics install -y libsafec libsafec-devel && sudo ln -s /usr/lib64/pkgconfig/safec-3.3.pc /usr/lib64/pkgconfig/libsafec.pc
-sudo dnf install -y gperftools-devel
+
 
 ## Install Snort3
 cd ~/sources && git clone https://github.com/snort3/snort3.git
-cd ~/sources/snort3 && export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH && export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig:$PKG_CONFIG_PATH && export CFLAGS="-O3" && export CXXFLAGS="-O3 -fno-rtti" && ./configure_cmake.sh --prefix=/usr/local/snort --enable-tcmalloc && cd ~/sources/snort3/build && sudo make -j$(nproc) && sudo make -j$(nproc) install
+cd ~/sources/snort3 && export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH && export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig:$PKG_CONFIG_PATH && export CFLAGS="-O3" && export CXXFLAGS="-O3 -fno-rtti" && ./configure_cmake.sh --prefix=/usr/local/snort --enable-tcmalloc && cd ~/sources/snort3/build && sudo make -j$(nproc) && sudo make -j$(nproc) install && /usr/local/snort/bin/snort -V
+sudo ln -s /usr/lib64/libtcmalloc.so.4 /lib/
+sudo ln -s /usr/local/lib/libdaq.so.3 /lib/
+sudo ldconfig
+sudo ln -s /usr/local/snort/bin/snort /usr/bin/snort
 
 ## Install Snort3 Extra
 cd ~/sources && git clone https://github.com/snort3/snort3_extra.git
 cd ~/sources/snort3_extra && export PKG_CONFIG_PATH=/usr/local/snort/lib64/pkgconfig:$PKG_CONFIG_PATH && ./configure_cmake.sh --prefix=/usr/local/snort/extra && cd ~/sources/snort3_extra/build && sudo make -j$(nproc) && sudo make -j$(nproc) install
 
 ## Check Snort3 version
-/usr/local/snort/bin/snort -V
+snort -V
